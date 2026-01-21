1113103a73de3a2641e9c91455652850e5d1b246
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-01-21 22:21:38 +0000

    exhibitions and contacts

--- db/seed.js
+++ db/seed.js
@@ -17,15 +17,41 @@ await db.query(`CREATE TABLE artists(
 await db.query(`CREATE TABLE artworks(
     artwork_id SERIAL PRIMARY KEY,
     artist_id SERIAL FOREIGN KEY,
-    title VARCHAR(20),
+    title VARCHAR(20) NOT NULL,
     year_created INT,
     artwork_type  VARCHAR(10),
-    medium VARCHAR(20),
+    medium VARCHAR(255),
     description TEXT,
     price INT,
     status VARCHAR(10),
     VAT status VARCHAR(23),
     edition INT
     );`);
+
+//create exhibitions table
+await db.query(`CREATE TABLE exhibitions(
+    exhibition_id SERIAL PRIMARY KEY,
+    gallery_id SERIAL FOREIGN KEY,
+    exhibition_name VARCHAR(50) NOT NULL,
+    start_date DATE,
+    end_date DATE,
+    description TEXT,
+     );`);
+
+//create contacts table
+await db.query(`CREATE TABLE contacts(
+    contact_id SERIAL PRIMARY KEY,
+    contact_type VARCHAR(50), -- person | organization
+    contact_name VARCHAR(255) NOT NULL,
+    email VARCHAR(50),
+    phone_number VARCHAR(50),
+    notes TEXT
+    );`);
+
+//create roles table
+await db.query(`CREATE TABLE roles(
+    id SERIAL PRIMARY KEY,
+    role_name VARCHAR(50) NOT NULL
+);`)
 
 }
