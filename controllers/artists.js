94b7646eae9ac2bc3152ea429cbebefbd57467c5
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-01-30 23:23:57 +0000

    artist controller

---
+++ controllers/artists.js
@@ -0,0 +1,17 @@
+const { fetchArtist } =  require("../models/artist");
+
+exports.getArtists = async (req, res, next) => {
+
+    const artists = await fetchArtists();
+
+    if (!artists.length) {
+        return res.status(200).send({ artists: [] });
+        }
+
+
+        return res.status(200).send({ artists });
+};
+
+
+
+
