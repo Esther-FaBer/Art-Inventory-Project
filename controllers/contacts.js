1a64cb9b81ec2a5a5d1d06972f27088778d07e7b
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-02-03 22:15:32 +0000

    controllers_contacts

---
+++ controllers/contacts.js
@@ -0,0 +1,13 @@
+const { fetchContacts } =  require("../models/contacts");
+
+exports.getContacts = async (req, res, next) => {
+
+    const contacts = await fetchContacts();
+
+    if (!contacts.length) {
+        return res.status(200).send({ contacts: [] });
+        }
+
+
+        return res.status(200).send({ contacts });
+};
