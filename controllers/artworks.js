0a0e1652c8fd874bd571014bdfcb6d3d9403ad9c
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-02-04 22:44:30 +0000

    controller_artwork try catch

---
+++ controllers/artworks.js
@@ -0,0 +1,15 @@
+const { fetchArtworks} =  require("../models/artworks");
+
+exports.getArtworks = async (req, res, next) => {
+
+    const artworks = await fetchArtworks();
+    try {
+        if(!artworks ||!artworks.length) {
+            return res.status(200).send({artworks: [] });
+        }
+
+            return res.status(200).send({ artworks });
+
+    } catch (error) {
+        next(error); // pass the error to the errors file
+}}
