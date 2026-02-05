2912627edb643ae4754f907421e091fc2d8dd12b
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-02-05 22:11:43 +0000

    artwork by Id

--- models/artworks.js
+++ models/artworks.js
@@ -1,5 +1,7 @@
 const db = require("../db/connection");
 
+
+//get all artworks
 exports.fetchArtworks = async () => {
     try {
         const {rows: artworks } = await db.query(
@@ -15,5 +17,24 @@ exports.fetchArtworks = async () => {
         console.error("error fetchin artworks:",  error);
         throw error;
     }
-
 };
+
+//get artwork by ID
+
+exports.fetchArtworkbyID = async (artworkId) => {
+    try {
+        const { rows: artworks } = await db.query(
+            `SELECT artwork_id, artist_id, title, year_created, 
+                artwork_type, medium, description, price, 
+                status, vat_status, edition, artist_name
+            FROM artworks
+            WHERE artwork_id = $1`,
+            [artworkId]
+        );
+        return artworks[0]; 
+
+    } catch (error) {
+        console.log('Error fetching artwork', error);
+        throw error;
+    }
+}
