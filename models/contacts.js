47917f9dfea3cbe0439783f0bfd1c519c5ed2d70
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-02-07 22:06:47 +0000

    contacts

--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,19 @@
 const db = require("../db/connection");
 
 exports.fetchContacts = async () => {
-    const { rows: contacts} = await db.query(
-        `SELECT contact_id, contact_type, contact_name,
-            email, phone_number, notes
-        FROM contacts
-        ORDER BY contact_name ASC`);
-    
-    return contacts;
 
+    try{
+
+        const { rows: contacts } = await db.query(
+            `SELECT contact_id, contact_type, contact_name,
+                email, phone_number, notes
+            FROM contacts
+            ORDER BY contact_name ASC`);
+    
+        return contacts;
+
+        } catch(error) {
+            console.log("Error fetching contacts", error);
+            throw error;
+        }
 };
