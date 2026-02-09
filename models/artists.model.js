c45469fae6eef9d954573b315446db45a6cbecaa
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-02-09 22:29:52 +0000

    artist by id

--- models/artists.model.js
+++ models/artists.model.js
@@ -10,5 +10,17 @@ exports.fetchArtists = async () => {
         ORDER BY artist_name ASC`);
     
     return artists;
+};
 
+// fetch artist by ID
+exports.fetchArtistById = async (artistId) => {
+    const { rows: artists } = await db.query(
+        `SELECT artist_id, artist_name, birth_year, death_year, 
+                nationality, biography
+         FROM artists
+         WHERE artist_id = $1`,
+        [artistId]
+    );
+    
+    return artists[0]; // Returns undefined if not found
 };
