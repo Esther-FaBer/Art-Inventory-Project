5fb744dbae4a60e94453ffd6bb395f1df24e5ae3
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-02-27 22:17:19 +0000

    app

--- app.js
+++ app.js
@@ -3,14 +3,22 @@ const cors = require("cors");
 
 //to import all routes
 const artistRoutes = require("./routes/artist.routes");
+const artworkRoutes = require("./routes/artworkRoutes");
+const galleryRoutes = require("./routes/galleryRoutes");
+const contactRoutes = require("./routes/contactRoutes");
+const roleRoutes = require("./routes/roleRoutes");
 
+// Import error handlers
 
 
 //to create Express app
 const app = express();
 
+
+//middleware
+app.use(cors());
 app.use(express.json());
 
-
+//create routes
 
 module.exports = app;
