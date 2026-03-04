eb4977ac08dad4cedccc8dcaa48e31edfe04af5d
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-03-04 22:43:50 +0000

    pool

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,22 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
 
-require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+require("dotenv").config({ 
+    path: `${__dirname}/../.env.${ENV}` 
+});
+
+if (!process.env.PGDATABASE) {
+    throw new Error('PGDATABASE not set');
+}
+
+const pool = new Pool({
+    host: process.env.PGHOST || 'localhost',
+    port: process.env.PGPORT || 5432,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    max: 10,
+
+});
 
 module.exports = pool;
