82ca59268f3954b69e520e3209e01c5bb24e4d54
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-05-30 23:25:25 +0100

    errors

--- errors.js
+++ errors.js
@@ -1,10 +1,11 @@
 exports.handlePathNotFound = (req, res, next) => {
-    res.status(404).send({msg:"Not found."});
+    const error = new Error('Not found');
+    error.statusCode = 404;
+    next(error);
 };
 
 exports.handleBadRequests = (err, req, res, next) => {
     const codes = ["23502", "22P02", "23503", "23505"];
-
     if(codes.includes(err.code)) {
         res.status(400).send({ msg:"Bad request."});
     } else {
